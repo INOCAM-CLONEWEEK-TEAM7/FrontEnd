bb938c442777d9c821710d18ba43f4308f23f644
Yongjin An <yongjin5352@gmail.com>
2023-07-24 14:19:39 +0900

    feat: 회원가입 페이지 조금완료, useValidateInput 훅 만듬

--- src/components/loginPage/EmailLogin.jsx
+++ src/components/loginPage/EmailLogin.jsx
@@ -1,23 +1,27 @@
+import useValidateInput from "../../hooks/useValidateInput"
 import ChangeOnHoverButton from "../common/styles/ChangeOnHoverButton"
 import StyledInput from "../common/styles/StyledInput"
 import EmailLoginForm from "./styles/EmailLoginForm"
 import UnderlinedLink from "./styles/UnderlinedLink"
 
 function EmailLogin() {
+  const [email, handleEmailOnChange, emailValid, setEmailValid] = useValidateInput('email');
+  const [password, handlePwOnChange, passwordValid, setPwValid] = useValidateInput('password');
+
+  
   return (
     <EmailLoginForm>
-      <StyledInput placeholder="이메일" $margin='1rem 0'/>
-      <StyledInput placeholder="비밀번호" $margin='1rem 0'/>
+      <StyledInput type="email" placeholder="이메일" $margin='1rem 0' value={email}/>
+      <StyledInput type='password' placeholder="비밀번호" $margin='1rem 0' value={password}/>
       <div>
         <UnderlinedLink to='/forgot'>비밀번호를 잊으셨나요?</UnderlinedLink>
       </div>
-      <ChangeOnHoverButton $margin='2rem 0' $width='100%'>
+      <ChangeOnHoverButton onClick={(e)=>{e.preventDefault()}}onSubmit={(e)=>{console.log(1111)}} $margin='2rem 0' $width='100%'>
         로그인
       </ChangeOnHoverButton>
       <div className='centered'>
         <UnderlinedLink to='/signup'>회원가입하기</UnderlinedLink>
       </div>
-
     </EmailLoginForm>
   )
 }
